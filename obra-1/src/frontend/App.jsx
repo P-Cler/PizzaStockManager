ae52b5aa662c81373453d3f68f85d9ec7561e0d2
Aline Mielli <alinemielli@gmail.com>
2025-07-09 20:46:16 -0300

    inclusao da estrutura base para o frontend

---
+++ obra-1/src/frontend/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+// import { Toaster } from "sonner";
+import { AppRoutes } from "./router/routes";
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+      {/* <Toaster richColors position="top-center" /> */}
+    </>
+  );
+}
+
+export default App;
