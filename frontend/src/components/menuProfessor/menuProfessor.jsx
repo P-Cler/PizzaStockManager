4cde6f8dcf053fee1934afd9ec6839567990e5a3
Aline Mielli <alinemielli@gmail.com>
2025-07-11 13:27:29 -0300

    ajustes de estilo na configuracao do jogo e menu lateral

--- frontend/src/components/menuProfessor/menuProfessor.jsx
+++ frontend/src/components/menuProfessor/menuProfessor.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import {
   Presentation,
   User,
-  BarChart,
+  MonitorCog,
   BookMarked,
-  CalendarDays,
-  Sandwich
+  ChartColumn,
+  Pizza
 } from "lucide-react";
 import styles from "@/components/menuProfessor/menuProfessor.module.css";
 
@@ -15,14 +15,14 @@ export default function MenuLateralProfessor() {
     <aside className={styles.menuLateral}>
       <div className={styles.cabecalhoMenu}>
         <Presentation size={40} />
-        <h3>Olá, Professor Roberto!</h3>
+        <h3>Olá, Professor Carlos!</h3>
       </div>
       <nav className={styles.navegacao}>
         <ul>
           <li>
             <Link to="/professor/parametrizacao" className={styles.linkMenu}>
-              <BarChart size={24} />
-              <span>Parametrização do jogo</span>
+              <MonitorCog size={24} />
+              <span>Configuração do jogo</span>
             </Link>
           </li>
           <li>
@@ -33,7 +33,7 @@ export default function MenuLateralProfessor() {
           </li>
           <li>
             <Link to="/professor/ingredientes" className={styles.linkMenu}>
-              <Sandwich size={24} />
+              <Pizza size={24} />
               <span>Cadastro de Ingredientes</span>
             </Link>
           </li>
@@ -45,7 +45,7 @@ export default function MenuLateralProfessor() {
           </li>
           <li>
             <Link to="/professor/resultados" className={styles.linkMenu}>
-              <CalendarDays size={24} />
+              <ChartColumn size={24} />
               <span>Resultados</span>
             </Link>
           </li>
